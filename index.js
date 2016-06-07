e763054f9fcbc6412af84f58a6eee24693ce4523
ankihg <hagelin.annika@gmail.com>
2016-06-07 11:02:12 -0700

    some close-out with execs and sander

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const addBranches = require( './lib/add-branches' );
 const hooks = require( './lib/ci-hooks' );
 
-const repoName = process.argv[2];
+const repoName = process.argv[2] || 'respond-accordingly';
 
-if( !repoName ) throw new Error( 'repo name must be provided' );
+// if( !repoName ) throw new Error( 'repo name must be provided' );
 
 // addBranches( repoName )
 // 	.then( () => console.log( 'branches created' ) )
@@ -16,5 +16,5 @@ if( !repoName ) throw new Error( 'repo name must be provided' );
 	// 	.catch( err => console.log( 'FAIL', err.message ) );
 
 
-console.log('plz respond');
+// console.log('plz respond');
 require('./lib/close-out')(repoName);
